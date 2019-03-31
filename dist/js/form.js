8379074dec2ec9d0ead65a5f5d3d7c7e21beebfa
Davina Leong <davina.leong.sy@gmail.com>
2019-04-01 00:05:38 +0800

    Highlight form fields for error/success

--- dist/js/form.js
+++ dist/js/form.js
@@ -1,40 +1,161 @@
 /// F. Form
 console.log('form.js loaded')
 
-/// F.1. Elements
-const alertForm = {
-  id: '#alert-form',
-  element: null
-}
 
-const formContact = {
-  id: '#form-contact',
-  element: null
-}
-
-
-/// F.2. document.ready
+/// F.1. document.ready
 $(document).ready(function() {
-  alertForm.element = $(alertForm.id)
-  formContact.element = $(formContact.id)
+
+  const alertForm = {
+
+    selector: '#alert-form',
+    element: null
+
+  }
+
+  const formContact = {
+
+    selector: '#form-contact',
+    element: null
+
+  }
+
+  const formField = {
+
+    selector: '.form-field'
+
+  }
+
+  const formFields = {
+
+    'name': {
+      name: 'name',
+      selector: "input[name='name']",
+      element: null
+    }, 
+    'email': {
+      name: 'email',
+      selector: "input[name='email']",
+      element: null
+    },
+    'mobile': {
+      name: 'mobile',
+      selector: "input[name='mobile']",
+      element: null
+    },
+    'subject': {
+      name: 'subject',
+      selector: "select[name='subject']",
+      element: null
+    },
+    'message': {
+      name: 'message',
+      selector: "textarea[name='message']",
+      element: null
+    }
+
+  }
+
+
+  alertForm.element = $(alertForm.selector)
+  formContact.element = $(formContact.selector)
+  Object.keys(formFields).map(key => {
+    formFields[key].element = $(formFields[key].selector)
+  })
 
   hide(alertForm.element)
 
-  submitForm(formContact.element)
+  submitForm(formContact.element, formFields, formField)
+
 })
 
 
-/// F.3. Helper functions
-function show(jqElement) {
-  jqElement.show()
+/// F.2. Helper functions
+function show(element) {
+
+  element.show()
+
 }
 
-function hide(jqElement) {
-  jqElement.hide()
+function hide(element) {
+
+  element.hide()
+
+}
+
+function success(elementTypes, elementType, element) {
+
+  switch (elementType) {
+    case elementTypes.formField:
+      element.parent('.form-field')
+        .addClass('form-success')
+        .removeClass('form-error')
+      break
+
+    case elementTypes.alert:
+      element
+        .addClass('alert-col-success')
+        .removeClass('alert-col-danger')
+      break
+
+    default:
+      break
+  }
+
+}
+
+function error(elementTypes, elementType, element) {
+
+  switch (elementType) {
+    case elementTypes.formField:
+      element.parent('.form-field')
+        .addClass('form-error')
+        .removeClass('form-success')
+      break
+
+    case elementTypes.alert:
+      element
+        .addClass('alert-col-error')
+        .removeClass('alert-col-success')
+      break
+
+    default:
+      break
+  }
+
 }
 
 
-/// F.4. Form functions
+/// F.3. Form functions
+function submitForm(form, fields, fieldParent) {
+
+  const elementTypes = {
+    formField: 'formField',
+    alert: 'alert'
+  }
+
+  $(form).submit(function(event) {
+    event.preventDefault()
+
+    const errors = validateForm(form, fields)
+
+    // Give all fields success classes
+    Object.keys(fields).map(key => {
+      success(elementTypes, elementTypes.formField, fields[key].element)
+    })
+
+    if (errors) {
+      
+      Object.keys(errors).map(errKey => {
+        error(elementTypes, elementTypes.formField, fields[errKey].element)
+      })
+
+    } else {
+      sendEmail()
+    }
+  })
+
+}
+
 function validateForm(form) {
   const formValidateConstraints = {
     name: {
@@ -69,14 +190,9 @@ function validateForm(form) {
     }
   }
 
-  console.log(validate.collectFormValues(form))
-  console.log(validate(validate.collectFormValues(form), formValidateConstraints))
+  return validate(validate.collectFormValues(form), formValidateConstraints)
 }
 
-function submitForm(form) {
-  $(form).submit(function (e) {
-    event.preventDefault()
-
-    validateForm()
-  })
+function sendEmail() {
+  console.log('TODO: Send Email')
 }
